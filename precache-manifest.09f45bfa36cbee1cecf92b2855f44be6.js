cddc3ecf2666030d0b6918e1effc8d1f131f7805
Alex Joshua <ialexjoshua@gmail.com>
2020-02-26 18:34:07 +0200

    Updates

---
+++ precache-manifest.09f45bfa36cbee1cecf92b2855f44be6.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e2110b2dfc8c7baeb7b6fab4475013",
+    "url": "/bluntjay/E-CommerceLuma-/index.html"
+  },
+  {
+    "revision": "76783db0ff59198d156c",
+    "url": "/bluntjay/E-CommerceLuma-/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c442d98680913768c99e",
+    "url": "/bluntjay/E-CommerceLuma-/static/css/main.a8f2b8cd.chunk.css"
+  },
+  {
+    "revision": "76783db0ff59198d156c",
+    "url": "/bluntjay/E-CommerceLuma-/static/js/2.ac43b5ed.chunk.js"
+  },
+  {
+    "revision": "941ec06aab5eaa7b741d96e66ed82296",
+    "url": "/bluntjay/E-CommerceLuma-/static/js/2.ac43b5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c442d98680913768c99e",
+    "url": "/bluntjay/E-CommerceLuma-/static/js/main.7c0b7449.chunk.js"
+  },
+  {
+    "revision": "08fe38dc9a0bce8fe048",
+    "url": "/bluntjay/E-CommerceLuma-/static/js/runtime-main.386df674.js"
+  },
+  {
+    "revision": "63fbdf0fbec21847f7b6a370b24b638c",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Ball1.63fbdf0f.jpg"
+  },
+  {
+    "revision": "8272670a9d52133270da52eccfc58ec3",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Ball2.8272670a.jpeg"
+  },
+  {
+    "revision": "f9a78b11be605c59d0d0b8a78fc8a19c",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Ball3.f9a78b11.jpg"
+  },
+  {
+    "revision": "1a5bc9491873cbde40eda5d693f611cf",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bench1.1a5bc949.jpg"
+  },
+  {
+    "revision": "0025a12ed8e84974fb11431efdf43269",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bench2.0025a12e.jpg"
+  },
+  {
+    "revision": "6fa26764f16c9c3ee00b644c7ea6527b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bench3.6fa26764.jpg"
+  },
+  {
+    "revision": "2a745c42e7adf9fba6360b108086df4e",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bike1.2a745c42.jpg"
+  },
+  {
+    "revision": "1906ab07c3b46ca3d2062ea857d1306e",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bike2.1906ab07.jpg"
+  },
+  {
+    "revision": "f652d29f0939394cbc0ee43ba1ad24a4",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Bike3.f652d29f.jpg"
+  },
+  {
+    "revision": "42159d819b7f458ba753dc1c28fd06f6",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/BoxKit1.42159d81.jpg"
+  },
+  {
+    "revision": "a6300d614623896916c42f448ec8f20d",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/BoxKit2.a6300d61.jpg"
+  },
+  {
+    "revision": "306f9b476f4747eb861eac187089d188",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/BoxKit3.306f9b47.jpg"
+  },
+  {
+    "revision": "d6138ac44ebf1af9d0e746d547a403c0",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/ColourTshirtStock.d6138ac4.jpg"
+  },
+  {
+    "revision": "0b30516a4a0ca8838cabcb6d6851e75d",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleFitness.0b30516a.jpg"
+  },
+  {
+    "revision": "1a74eabf4fa69b620da8aadcfe2821ef",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTank1.1a74eabf.jpg"
+  },
+  {
+    "revision": "ef062392d23b659a9a936d4e91b0ee95",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTank2.ef062392.jpg"
+  },
+  {
+    "revision": "de902715f48f7bd1a9e41805f9dd3341",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTank3.de902715.jpg"
+  },
+  {
+    "revision": "88b39aa4516cafd3ec37b817d458fb20",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTrain1.88b39aa4.jpg"
+  },
+  {
+    "revision": "9dc15955c4914af37644169f704d316b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTrain2.9dc15955.jpg"
+  },
+  {
+    "revision": "eca39218844470bee8996ad02400a1bf",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/FemaleTrain3.eca39218.jpg"
+  },
+  {
+    "revision": "d2248d41d59260f1006b1e1ade937eb0",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Fitbit1.d2248d41.jpg"
+  },
+  {
+    "revision": "255bd11995095e34ed781d04bca00e72",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Fitbit2.255bd119.jpg"
+  },
+  {
+    "revision": "5cf9e5a2706755deea7d90e46d8b65b5",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Fitbit3.5cf9e5a2.jpg"
+  },
+  {
+    "revision": "b42c9ecbf790bf0311b97175e03bc932",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymBag1.b42c9ecb.jpg"
+  },
+  {
+    "revision": "352eb13b29f053304882d2d3a56b22b4",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymBag2.352eb13b.jpg"
+  },
+  {
+    "revision": "92817eef9fb4627f7211fe47569e8cd0",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymBag3.92817eef.jpg"
+  },
+  {
+    "revision": "53ffab36eec1282fe02f956734064108",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymShorts1.53ffab36.jpg"
+  },
+  {
+    "revision": "cb59bb145e76354f0e2659e24847b8f3",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymShorts2.cb59bb14.jpg"
+  },
+  {
+    "revision": "41ecf486208e4cdb1e1d374bd2b5df0c",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/GymShorts3.41ecf486.jpg"
+  },
+  {
+    "revision": "3105be0a2c07cfd8887896d58ff642d8",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Leggings1.3105be0a.jpg"
+  },
+  {
+    "revision": "dfb2ab9b6f4397276de6ddda66b00d9c",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Leggings2.dfb2ab9b.jpg"
+  },
+  {
+    "revision": "8025b563105dae6e9291d09acd790c9b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Leggings3.8025b563.jpg"
+  },
+  {
+    "revision": "d26782e35e901e3b0279314c2f31b991",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/LumaLogo.d26782e3.svg"
+  },
+  {
+    "revision": "dbf78e70fb0a5752a68cfedd1b3764d2",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/MaleTrain1.dbf78e70.jpg"
+  },
+  {
+    "revision": "c38e21503a2b3e614ebe0c915ea27685",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/MaleTrain2.c38e2150.jpg"
+  },
+  {
+    "revision": "bfc7167f1f4b3f577d209cd8d889c937",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/MaleTrain3.bfc7167f.jpg"
+  },
+  {
+    "revision": "fcdc2c2e555d842eda891b1e72d634a5",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/ManBeach.fcdc2c2e.jpg"
+  },
+  {
+    "revision": "ac7a029274dc5b02fcc144f694bf2a0f",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Shorts1.ac7a0292.jpg"
+  },
+  {
+    "revision": "c06e5328870a0fa0120bd516272828e0",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Shorts2.c06e5328.jpg"
+  },
+  {
+    "revision": "5ea7d95a2876ca8f9105f6386aab3f20",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Shorts3.5ea7d95a.jpg"
+  },
+  {
+    "revision": "183c79c441d8058386c989e15df8ed4b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportBra1.183c79c4.jpg"
+  },
+  {
+    "revision": "fd19db006f31981989413568973ff993",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportBra2.fd19db00.jpg"
+  },
+  {
+    "revision": "b86d182fad247410c0502a865deb1db9",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportBra3.b86d182f.jpg"
+  },
+  {
+    "revision": "a306e474ef8e5da62d1771d89cfa69aa",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportJack1.a306e474.jpg"
+  },
+  {
+    "revision": "68ed02834973226f759b2d62c1a05c5b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportJack2.68ed0283.jpg"
+  },
+  {
+    "revision": "834d7c8af471425f7a25d59735048025",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/SportJack3.834d7c8a.jpg"
+  },
+  {
+    "revision": "5620cddffdb7889f176e06929b814411",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Sweatpants1.5620cddf.jpg"
+  },
+  {
+    "revision": "db525b6fc68a2b3dc2f97fe01e339089",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Sweatpants3.db525b6f.jpg"
+  },
+  {
+    "revision": "144a3a53a20fe6cce1f422bc9ddf38db",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Treadmill1.144a3a53.jpg"
+  },
+  {
+    "revision": "381b34aa3b85b3dade7f66f13b945703",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Treadmill2.381b34aa.jpg"
+  },
+  {
+    "revision": "cf3802360a70361a1bf130c57da369a1",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Treadmill3.cf380236.jpg"
+  },
+  {
+    "revision": "2cfc916984374067898bdeccd2518611",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/WaterBottle2.2cfc9169.jpg"
+  },
+  {
+    "revision": "e60ae2008a51fe34dceee918cf8bb85f",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/WaterBottle3.e60ae200.jpg"
+  },
+  {
+    "revision": "b6c8a7b78307d05ff6737537a33f35c4",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Weights1.b6c8a7b7.jpg"
+  },
+  {
+    "revision": "88dda1cffac83d06943f81e16436574b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Weights2.88dda1cf.jpg"
+  },
+  {
+    "revision": "41449d8d1319b69e61ec5a75df35f707",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Weights3.41449d8d.jpg"
+  },
+  {
+    "revision": "f640bfe28d9b2114861fbd0eb29180b9",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/WomanBeach.f640bfe2.jpg"
+  },
+  {
+    "revision": "3090964dc664bf75b85501769ea6dd57",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/WomanNature1.3090964d.jpg"
+  },
+  {
+    "revision": "856dba9d4e40f56048fe5a10e34f1a8b",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Yoga1.856dba9d.jpg"
+  },
+  {
+    "revision": "06a5bd18fdcc7f5dbd4099d0e111cb73",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Yoga2.06a5bd18.jpg"
+  },
+  {
+    "revision": "3a90a1890eeea94d3baf03ec68f3d9ff",
+    "url": "/bluntjay/E-CommerceLuma-/static/media/Yoga3.3a90a189.jpg"
+  }
+]);
